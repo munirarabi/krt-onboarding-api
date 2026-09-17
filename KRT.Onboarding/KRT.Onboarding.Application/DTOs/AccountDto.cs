7867c63861c89974cf4deca43e770e79d26db599
Munir Marques <munirarabi04@gmail.com>
2026-09-17 15:48:46 -0300

    fix(application): ajusting account class

--- KRT.Onboarding/KRT.Onboarding.Application/DTOs/AccountDto.cs
+++ KRT.Onboarding/KRT.Onboarding.Application/DTOs/AccountDto.cs
@@ -1,6 +1,12 @@
-﻿namespace KRT.Onboarding.Application.DTOs
+﻿using KRT.Onboarding.Domain.Enums;
+
+namespace KRT.Onboarding.Application.DTOs
 {
     public class AccountDto
     {
+        public Guid Id { get; set; }
+        public string HolderName { get; set; } = string.Empty;
+        public string Cpf { get; set; } = string.Empty;
+        public AccountStatus Status { get; set; }
     }
 }
