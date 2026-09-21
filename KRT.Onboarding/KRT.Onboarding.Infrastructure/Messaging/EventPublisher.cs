8e697b0ca156dc3a99c462c93112d5e8dab8200f
munirarabi <munirarabi04@gmail.com>
2026-09-20 22:42:12 -0300

    feat: Add conditional to event publisher

--- KRT.Onboarding/KRT.Onboarding.Infrastructure/Messaging/EventPublisher.cs
+++ KRT.Onboarding/KRT.Onboarding.Infrastructure/Messaging/EventPublisher.cs
@@ -1,4 +1,5 @@
 ﻿using KRT.Onboarding.Application.Interfaces.Messaging;
+using KRT.Onboarding.Domain.Events;
 
 namespace KRT.Onboarding.Infrastructure.Messaging
 {
@@ -6,11 +7,31 @@ namespace KRT.Onboarding.Infrastructure.Messaging
     {
         public Task PublishAsync<T>(T message, CancellationToken cancellationToken)
         {
-            /*
-             Aqui seria onde ocorreria a conexão com o serviço de mensageria e a publicação da mensagem seria feita
-             */
+            // Aqui seria onde ocorreria a conexão com o serviço de mensageria e a publicação da mensagem seria feita
+            // Simula a publicação de sucesso do evento sem enviar para um broker oficial
 
-            // Simula a publicação de sucesso do evento sem enviar para um broker oficial.
+            switch (message)
+            {
+                case AccountCreatedEvent:
+                    // Publicar evento de criação
+                    Console.WriteLine("Publicar evento de create para as áreas");
+                    break;
+
+                case AccountUpdatedEvent:
+                    // Publicar evento de atualização
+                    Console.WriteLine("Publicar evento de update para as áreas");
+                    break;
+
+                case AccountDeletedEvent:
+                    // Publicar evento de exclusão
+                    Console.WriteLine("Publicar evento de delete para as áreas");
+                    break;
+
+                default:
+                    throw new InvalidOperationException(
+                        $"Event type '{typeof(T).Name}' não é suportado");
+            }
+
             return Task.CompletedTask;
         }
     }
